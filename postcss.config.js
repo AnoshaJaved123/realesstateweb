2d48cbd52c997c8482d34bdc5950c81cb92e7346
AnoshaJaved1991 <anosha.lges.ag@gmail.com>
2022-09-07 17:55:07 +0500

    new

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,6 @@
-// module.exports = {
-//   plugins: {
-//     tailwindcss: {},
-//     autoprefixer: {},
-//   },
-//   plugins: [
-//     require('tailwindcss'),
-//     require('autoprefixer'),
-//   ]
-// }
-
 module.exports = {
   plugins: {
-    'postcss-import': {},
     tailwindcss: {},
-    'postcss-preset-env': { stage: 2 },
+    autoprefixer: {},
   },
-};
+}
