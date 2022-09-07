728ec704c3b9897fe5628d37d76d8386086aae6a
AnoshaJaved1991 <anosha.lges.ag@gmail.com>
2022-09-07 17:35:14 +0500

    new changes

--- pages/_document.js
+++ pages/_document.js
@@ -11,10 +11,15 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
             rel="stylesheet"
           />
+          <link rel="stylesheet" href="https://unpkg.com/flowbite@1.4.5/dist/flowbite.min.css" />
+          <link href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css" rel="stylesheet" />
+          <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet" />
         </Head>
         <body>
           <Main />
           <NextScript />
+          <script src="https://unpkg.com/flowbite@1.4.4/dist/flowbite.js"></script>
+
         </body>
       </Html>
     )
