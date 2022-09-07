615534b02db66d9335bbb4c3e3f800fa99bbe872
AnoshaJaved1991 <anosha.lges.ag@gmail.com>
2022-09-07 16:40:48 +0500

    added new file document.js

--- next.config.js
+++ next.config.js
@@ -5,23 +5,6 @@ const nextConfig = {
   images: {
     domains: [`${process.env.NEXT_PUBLIC_HOST}`,'lh3.googleusercontent.com','icon-library.com','realestatebuketwebdev.s3.ap-northeast-3.amazonaws.com'], //Domain of image host
   },
-  webpack: function (config) {
-    const originalEntry = config.entry;
-
-    config.entry = async () => {
-      const entries = await originalEntry();
-
-      if (
-        entries["main.js"] &&
-        !entries["main.js"].includes("./src/polyfills.js")
-      ) {
-        entries["main.js"].unshift("./src/polyfills.js");
-      }
-      return entries;
-    };
-
-    return config;
-  },
 }
 
 module.exports = nextConfig
