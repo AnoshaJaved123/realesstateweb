84627bfa7c4d360672ac69ca5135a3ec4677f92b
AnoshaJaved1991 <anosha.lges.ag@gmail.com>
2022-09-07 18:30:31 +0500

    change

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
   const [type, setType] = useState("Homes")
   const [subtype, setSubtype] = useState("House")
   const [city, setCity] = useState("Lahore")
-  
+
 
 
   return (
@@ -16,7 +16,15 @@ export default function Home() {
       <Head>
         <title>Real Estate App</title>
         <link rel="icon" href="/icon.png" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
+          rel="stylesheet"
+        />
+        <link rel="stylesheet" href="https://unpkg.com/flowbite@1.4.5/dist/flowbite.min.css" />
+        <link href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css" rel="stylesheet" />
+        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet" />
       </Head>
+      <Script src="https://unpkg.com/flowbite@1.4.4/dist/flowbite.js"/>
       <div className='px-0 h-80vh w-full object-fill' style={{
         backgroundSize: "100% 100%", backgroundImage: `url(${'/h1.jpg'})`,
       }}>
@@ -26,14 +34,14 @@ export default function Home() {
               <div className="backdrop-opacity-10 backdrop-invert bg-white/80 py-3  flex lg:w-2/3 w-full sm:flex-row flex-col mx-auto px-8 sm:space-x-4 sm:space-y-0 space-y-4 sm:px-3 ">
                 <div className="relative flex-grow ">
                   <label htmlFor="name" className="leading-7 text-sm font-semibold text-black">Select Category</label>
-                  <select value={category} onChange={e => setcategory(e.target.value) } type="text" name="category" className="rounded border appearance-none border-gray-300 py-1 focus:outline-none focus:ring-2 focus:ring-stone-200 focus:border-stone-700 text-black pl-3 pr-10 w-full">
+                  <select value={category} onChange={e => setcategory(e.target.value)} type="text" name="category" className="rounded border appearance-none border-gray-300 py-1 focus:outline-none focus:ring-2 focus:ring-stone-200 focus:border-stone-700 text-black pl-3 pr-10 w-full">
                     <option value={'Rent'} >Rent</option>
                     <option value={'Sale'} >Sale</option>
                   </select>
                 </div>
                 <div className="relative flex-grow ">
                   <label htmlFor="name" className="leading-7 text-sm font-semibold text-black">Select Property Type</label>
-                  <select value={type} onChange={e => setType(e.target.value) } type="text" name="type" className="rounded border appearance-none border-gray-300 py-1 focus:outline-none focus:ring-2 focus:ring-stone-200 focus:border-stone-700 text-black pl-3 pr-10 w-full">
+                  <select value={type} onChange={e => setType(e.target.value)} type="text" name="type" className="rounded border appearance-none border-gray-300 py-1 focus:outline-none focus:ring-2 focus:ring-stone-200 focus:border-stone-700 text-black pl-3 pr-10 w-full">
                     <option value={'Homes'} >Homes</option>
                     <option value={'Plots'} >Plots</option>
                     <option value={'Commercial'} >Commercial</option>
@@ -42,7 +50,7 @@ export default function Home() {
                 <div className="relative flex-grow ">
                   <div>
                     <label htmlFor="name" className="leading-7 text-sm font-semibold text-black">Select Subtype</label>
-                    {type === 'Homes' && <> <select value={type} onChange={e => setSubtype(e.target.value) }  type="text" name="subtype" className="rounded border appearance-none border-gray-300 py-1 focus:outline-none focus:ring-2 focus:ring-stone-200 focus:border-stone-700 text-black pl-3 pr-10 w-full">
+                    {type === 'Homes' && <> <select value={type} onChange={e => setSubtype(e.target.value)} type="text" name="subtype" className="rounded border appearance-none border-gray-300 py-1 focus:outline-none focus:ring-2 focus:ring-stone-200 focus:border-stone-700 text-black pl-3 pr-10 w-full">
                       <option value={'House'} >House</option>
                       <option value={'Flat'} >Flat</option>
                       <option value={'Upper Portion'} >Upper Portion</option>
@@ -52,7 +60,7 @@ export default function Home() {
                       <option value={'PentHouse'} >PentHouse</option>
 
                     </select></>}
-                    {type === 'Plots' && <> <select value={type} onChange={e => setSubtype(e.target.value) }  type="text" name="subtype" className="rounded border appearance-none border-gray-300 py-1 focus:outline-none focus:ring-2 focus:ring-stone-200 focus:border-stone-700 text-black pl-3 pr-10 w-full">
+                    {type === 'Plots' && <> <select value={type} onChange={e => setSubtype(e.target.value)} type="text" name="subtype" className="rounded border appearance-none border-gray-300 py-1 focus:outline-none focus:ring-2 focus:ring-stone-200 focus:border-stone-700 text-black pl-3 pr-10 w-full">
                       <option value={'Residential Plot'} >Residential Plot</option>
                       <option value={'Commercial Plot'} >Commercial Plot</option>
                       <option value={'Agricultural Land'} >Agricultural Land</option>
@@ -60,7 +68,7 @@ export default function Home() {
                       <option value={'Plot File'} >Plot File</option>
                       <option value={'Plot Form'} >Plot Form</option>
                     </select></>}
-                    {type === 'Commercial' && <> <select value={type} onChange={e => setSubtype(e.target.value) }  type="text" name="subtype" className="rounded border appearance-none border-gray-300 py-1 focus:outline-none focus:ring-2 focus:ring-stone-200 focus:border-stone-700 text-black pl-3 pr-10 w-full">
+                    {type === 'Commercial' && <> <select value={type} onChange={e => setSubtype(e.target.value)} type="text" name="subtype" className="rounded border appearance-none border-gray-300 py-1 focus:outline-none focus:ring-2 focus:ring-stone-200 focus:border-stone-700 text-black pl-3 pr-10 w-full">
                       <option value={'Office'} >Office</option>
                       <option value={'Shop'} >Shop</option>
                       <option value={'Warehouse'} >Warehouse</option>
@@ -72,7 +80,7 @@ export default function Home() {
                 </div>
                 <div className="relative flex-grow">
                   <label htmlFor="name" className="leading-7 text-sm font-semibold text-black">Select City</label>
-                  <select value={city} onChange={e => setCity(e.target.value) } type="text" name="city" className="rounded border appearance-none border-gray-300 py-1 focus:outline-none focus:ring-2 focus:ring-stone-200 focus:border-stone-700 text-black pl-3 pr-10 w-full">
+                  <select value={city} onChange={e => setCity(e.target.value)} type="text" name="city" className="rounded border appearance-none border-gray-300 py-1 focus:outline-none focus:ring-2 focus:ring-stone-200 focus:border-stone-700 text-black pl-3 pr-10 w-full">
                     <option value={'Lahore'} >Lahore</option>
                     <option value={'Karachi'} >Karachi</option>
                     <option value={'Islamabad'} >Islamabad</option>
@@ -96,12 +104,12 @@ export default function Home() {
                         subtype: subtype,
                         city: city,
 
-                      
+
                       }
                     }}
-                   
+
                   >
-                  <a> <button className="text-white mt-6 bg-stone-700 border-0 py-1 px-8 focus:outline-none hover:bg-stone-400  text-lg">Find</button></a></Link>
+                    <a> <button className="text-white mt-6 bg-stone-700 border-0 py-1 px-8 focus:outline-none hover:bg-stone-400  text-lg">Find</button></a></Link>
                 </div>
 
 
@@ -118,9 +126,12 @@ export default function Home() {
           <div className="container mx-auto px-4">
 
             <div>
-               <section className="py-8 px-4">
+              <section className="py-8 px-4">
                 <div className="flex flex-wrap -mx-4">
-                 
+                <div className="flex flex-col text-center w-full mb-5">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-stone-700">Real Estate Projects</h1>
+            <p className="lg:w-2/3 mx-auto leading-7elaxed text-black">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably havent heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
+          </div>
                   <div className="relative md:w-1/2 px-4 mb-8 md:mb-6">
                     <div className="absolute inset-0 z-10 bg-white text-center flex flex-col items-center justify-center opacity-0 hover:opacity-100 bg-opacity-50 duration-300">
                       <h1 className="tracking-wider text-3xl font-semibold text-black">For Rent</h1>
@@ -129,7 +140,7 @@ export default function Home() {
                     </div>
                     <div className="relative">
                       <div className=" flex flex-wrap content-center">
-                      <Image className="rounded shadow-md" src={'/h9.jpg'} width={768} height={400} alt='any' />
+                        <Image className="rounded shadow-md" src={'/h9.jpg'} width={768} height={400} alt='any' />
 
                       </div>
                     </div>
@@ -143,7 +154,7 @@ export default function Home() {
                     </div>
                     <div className="relative">
                       <div className=" flex flex-wrap content-center">
-                      <Image className="rounded shadow-md" src={'/h2.jpg'} width={768} height={400} alt='any' />
+                        <Image className="rounded shadow-md" src={'/h2.jpg'} width={768} height={400} alt='any' />
 
                       </div>
                     </div>
@@ -157,7 +168,7 @@ export default function Home() {
                     </div>
                     <div className="relative">
                       <div className=" flex flex-wrap content-center">
-                      <Image className="rounded shadow-md" src={'/h13.jpg'} width={768} height={400} alt='any' />
+                        <Image className="rounded shadow-md" src={'/h13.jpg'} width={768} height={400} alt='any' />
 
                       </div>
                     </div>
@@ -171,7 +182,7 @@ export default function Home() {
                     </div>
                     <div className="relative">
                       <div className=" flex flex-wrap content-center">
-                      <Image className="rounded shadow-md" src={'/h3.jpeg'} width={768} height={400} alt='any' />
+                        <Image className="rounded shadow-md" src={'/h3.jpeg'} width={768} height={400} alt='any' />
 
                       </div>
                     </div>
@@ -245,7 +256,7 @@ export default function Home() {
             <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
               <div className="h-full text-center">
                 {/* <Image  width={40} height={40} alt="testimonial" className="w-40 h-40 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://apiwp.thelocal.com/wp-content/uploads/2018/12/6d67730d16af04f3f956389d4cc244af808b8381c23b1e3d218ecd792de14fa8-616x431.jpg" /> */}
-                <Image  width={100} height={100} alt="testimonial" className="w-40 h-40 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src={'/u2.jpg'} />
+                <Image width={100} height={100} alt="testimonial" className="w-40 h-40 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src={'/u2.jpg'} />
 
                 <p className="leading-7elaxed">Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90s cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
                 <span className="inline-block h-1 w-10 rounded bg-stone-700 mt-6 mb-4" />
@@ -256,7 +267,7 @@ export default function Home() {
             <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
               <div className="h-full text-center">
                 {/* <Image width={40} height={40}  alt="testimonial" className="w-40 h-40 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" /> */}
-                <Image  width={100} height={100} alt="testimonial" className="w-40 h-40 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src={'/u1.jpg'} />
+                <Image width={100} height={100} alt="testimonial" className="w-40 h-40 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src={'/u1.jpg'} />
                 <p className="leading-7elaxed">Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90s cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
                 <span className="inline-block h-1 w-10 rounded bg-stone-700 mt-6 mb-4" />
                 <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm">ALPER KAMU</h2>
@@ -266,7 +277,7 @@ export default function Home() {
             <div className="lg:w-1/3 lg:mb-0 p-4">
               <div className="h-full text-center">
                 {/* <Image width={40} height={40}  alt="testimonial" className="w-40 h-40 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" /> */}
-                <Image  width={100} height={100} alt="testimonial" className="w-40 h-40 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src={'/u3.jpg'} />
+                <Image width={100} height={100} alt="testimonial" className="w-40 h-40 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src={'/u3.jpg'} />
                 <p className="leading-7elaxed">Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90s cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
                 <span className="inline-block h-1 w-10 rounded bg-stone-700 mt-6 mb-4" />
                 <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm">HENRY LETHAM</h2>
