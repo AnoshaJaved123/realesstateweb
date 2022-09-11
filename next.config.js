6eaf10cc8d13b14af6f495be623dd38caa77d6c7
AnoshaJaved1991 <anosha.lges.ag@gmail.com>
2022-09-11 13:22:32 +0500

    new change

--- next.config.js
+++ next.config.js
@@ -1,16 +1,13 @@
 /** @type {import('next').NextConfig} */
+const { i18n } = require('./next-i18next.config');
+
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: false,
+  i18n,
   images: {
     domains: [`${process.env.NEXT_PUBLIC_HOST}`,'lh3.googleusercontent.com','icon-library.com','anoshanewbuket2022.s3.eu-west-2.amazonaws.com'], //Domain of image host
   },
 }
 
 module.exports = nextConfig
-// // next.config.js
-// module.exports = {
-//   images: {
-//     domains: ['lh3.googleusercontent.com'], //Domain of image host
-//   },
-// }
